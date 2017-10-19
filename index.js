074ce2a8c6af196156d0f7a2aa6712a41c58054f
Dallas Read <dallas@excitecreative.ca>
2017-10-19 08:27:39 -0300

    localStorage fix.

--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 var data;
 
-if (window.localStorage && window.localStorage.getItem('calc')) {
-    data = JSON.parse(window.localStorage.getItem('calc'));
-}
+try {
+    if (window.localStorage) {
+        data = JSON.parse(window.localStorage.getItem('calc'));
+    }
 
-if (typeof data.debts === 'undefined') {
-    if (window.location.search.indexOf('debt') !== -1) {
-        var match = window.location.search.match(/debt=([^&$]+)/);
+    if (typeof data === 'undefined') {
+        if (window.location.search.indexOf('debt') !== -1) {
+            var match = window.location.search.match(/debt=([^&$]+)/);
 
-        if (match) {
-            data = JSON.parse(atob(decodeURIComponent(match[1])));
+            if (match) {
+                data = JSON.parse(atob(decodeURIComponent(match[1])));
+            }
         }
     }
-}
+} catch (e) {}
 
-if (typeof data.debts === 'undefined') {
+if (typeof data === 'undefined' || typeof data.debts === 'undefined') {
     data = {
         debts: [
             { name: 'Loan',  rate: 5,     principle: 29000, payment: 200 },
